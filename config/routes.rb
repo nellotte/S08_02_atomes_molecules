4218c7bdc27a1c13849620447cbfc7625aec784d
nellotte <nelly.guerin@gmail.com>
2023-11-14 21:54:59 +0100

    add atome and molecule index pages + users list

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
+  get 'static_pages/index_atome'
+  get 'static_pages/index_molecule'
   get 'static_pages/navbar'
   get 'static_pages/footer2' 
   get 'static_pages/big_banner'
   get 'static_pages/small_banner'
   get 'static_pages/authentification_form'
   get 'static_pages/event_card'
+  get 'static_pages/users_list'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -13,5 +15,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "static_pages#index"
+  root "static_pages#index_atome"
 end
